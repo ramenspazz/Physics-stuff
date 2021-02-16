ccf523ac210820a1dd68c67a5fc88996bb03bf7c
Dalton <kirisame.ramen@gmail.com>
2021-02-16 15:40:46 -0800

    Update std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -4,8 +4,8 @@ import numpy as np
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-
-n_bins = 0
+from astropy import modeling
+n_bins = 10
 
 def std_dev():
     try:
@@ -14,14 +14,17 @@ def std_dev():
         n = 0    
         # calc mean
         data_vec = []
-        n_bins = int(input("Enter the number of bins to use: ))
-        with open("data.txt") as f:
+        n_bins = int(input("enter number of bins: "))
+        with open("data4.txt") as f:
             content = f.readlines()
 
+        # Show the file contents line by line.
+        # We added the comma to print single newlines and not double newlines.
+        # This is because the lines contain the newline character '\n'.
         for line in content:
             temp_line = line.split()
             data_vec.append(int(temp_line[2])) 
-        
+
         data_vec = np.array(data_vec)
         mean = sum(data_vec) / len(data_vec)
         # calc stdev
@@ -35,8 +38,16 @@ def std_dev():
             if (item < mean - 2*temp) or (item > mean + 2*temp):
                 n = n + 1
         print("There are {} items outside of one standard deviation of the mean.".format(n))
+        fig, axs = plt.subplots(1,2)
+        
+        m = modeling.models.Gaussian1D(amplitude=2854, mean=mean, stddev=temp)
+        x = np.linspace(min(data_vec), max(data_vec), len(data_vec))
+        data = m(x)
+        data = data + np.sqrt(data) * np.random.random(x.size) - 0.5
+        data -= data.min()
+        axs[0].plot(x, data)
 
-        plt.hist(data_vec)
+        axs[1].hist(data_vec, n_bins)
         plt.show() 
 
     except Exception as e:
